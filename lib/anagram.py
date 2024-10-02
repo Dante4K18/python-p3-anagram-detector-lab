a6971104dc795b9538c8bac406c4248c8d1bd447
Dante4K18 <dan@dan>
2024-10-02 03:59:17 +0300

    completed-anagram-detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word: str):
+        self.word = word
+
+    def match(self, candidates: list) -> list:
+        # Sort the letters of the initialized word
+        sorted_word = sorted(self.word)
+        matches = []
+        
+        for candidate in candidates:
+            # Sort the letters of the candidate
+            if sorted(candidate) == sorted_word:
+                matches.append(candidate)
+
+        return matches
